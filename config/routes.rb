7d2e8954f1022328e967428900bb292889db3cc7
shunichisato26 <68091953+shunichisato26@users.noreply.github.com>
2021-03-23 10:16:39 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  root 'tweets#index'
+
+  devise_for :users
+  resources :users
+  resources :tweets
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
