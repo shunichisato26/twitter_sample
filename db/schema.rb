4f17ab842d9b1fa10d0c3ef1738d93e713b5bd88
shunichisato26 <68091953+shunichisato26@users.noreply.github.com>
2021-03-25 20:15:49 +0900

    ユーザーテーブルに「ユーザー名」カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_112313) do
+ActiveRecord::Schema.define(version: 2021_03_25_081920) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_03_21_112313) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
