caf4182859a9b8f5faab309969c9170c81a2a713
shunichisato26 <68091953+shunichisato26@users.noreply.github.com>
2021-03-26 16:34:43 +0900

    likeモデル生成、アソシエーション設定、コントローラー生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :tweets, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_tweets, through: :likes, source: :tweet
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'likes/create'
+  get 'likes/destroy'
   root 'tweets#index'
 
   devise_for :users
