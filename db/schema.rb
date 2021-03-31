2b26afe32db88daa04728b8c0d0796f336acae48
shunichisato26 <68091953+shunichisato26@users.noreply.github.com>
2021-03-31 14:20:39 +0900

    修正後のスキーマファイル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_113907) do
+ActiveRecord::Schema.define(version: 2021_03_26_061956) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "tweet_id", null: false
@@ -22,11 +22,11 @@ ActiveRecord::Schema.define(version: 2021_03_27_113907) do
   end
 
   create_table "tweets", force: :cascade do |t|
+    t.integer "user_id", null: false
     t.text "content"
-    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"user_id\", \"tweet\"", name: "index_tweets_on_user_id_and_tweet", unique: true
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_03_27_113907) do
 
   add_foreign_key "likes", "tweets"
   add_foreign_key "likes", "users"
+  add_foreign_key "tweets", "users"
 end
